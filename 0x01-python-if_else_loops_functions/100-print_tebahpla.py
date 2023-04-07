883b5e0d8200794a0758ac7ac5bce1a039073f71
Brukzeratsion <86562906+Brukzeratsion@users.noreply.github.com>
2023-04-07 14:08:12 +0300

    Update 100-print_tebahpla.py

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,5 +1,3 @@
 #!/usr/bin/python3
-
-""""Print the alphabet in reverse order alternating upper- and lower-case."""
-for c in range(ord('z'), ord('a') - 1, -1):
-    print("{:c}".format((c - (ord('a') - ord('A'))) if c % 2 else c), end='')
+for d in range(ord('z'), ord('a') - 1, -1):
+    print("{:d}".format((d - (ord('a') - ord('A'))) if d % 2 else d), end='')
